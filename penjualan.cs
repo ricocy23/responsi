9dc83041267d4071e90e4ac5f6b27d569f13b799
rico <101581565+ricocy23@users.noreply.github.com>
2022-07-12 12:37:04 +0700

    Create penjualan.cs

---
+++ penjualan.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Penjualan
+{
+    class Penjualan
+    {
+        public string Tanggal { get; set; }
+        public string Nota { get; set; }
+        public string Nama { get; set; }
+        public string Gender { get; set; }
+        public int Total { get; set; }
+    }
+}
